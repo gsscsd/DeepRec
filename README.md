36b8eb991dac86c2a113d73fb592afb63dd50edb
wangjiacun <wangjiacun@apple.com>
2021-08-19 18:00:24 +0800

    [Feature] update feature_columns

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # DeepRec
 
-deeprec is a deep learning frame
+> deeprec是一个开箱即用的深度推荐框架.
+
--- deeprec/features/feature_columns.py
+++ deeprec/features/feature_columns.py
@@ -1,4 +1,3 @@
-
 class BaseFeature(object):
 
     def __init__(self, name, dim, dtype, **kwargs):
@@ -6,26 +5,47 @@ class BaseFeature(object):
         self._dim = dim
         self._dtype = dtype
 
-    def get_name(self):
-        pass
+    @property
+    def name(self):
+        return self._name
 
-    def get_dim(self):
-        pass
+    @property
+    def dim(self):
+        return self._dim
 
-    def get_dtype(self):
-        pass
+    @property
+    def dtype(self):
+        return self._dtype
 
     def __str__(self):
         return "BaseFeature name: {0}, dim: {1}, dtype: {2}".format(self._name, self._dim, self._dtype)
 
 
 class DenseFeature(BaseFeature):
-    def __init__(self):
-        pass
-    pass
+    def __init__(self, name, dim, dtype, func, **kwargs):
+        self._func = func
+        super(DenseFeature, self).__init__(name, dim, dtype, **kwargs)
+
+    @property
+    def func(self):
+        return self._func
 
 
 class SparseFeature(BaseFeature):
-    def __init__(self):
-        pass
-    pass
+    def __init__(self, name, dim, dtype, use_hash, hash_size, emb_dim, **kwargs):
+        self._use_hash = use_hash
+        self._hash_size = hash_size
+        self._emb_dim = emb_dim
+        super(SparseFeature, self).__init__(name, dim, dtype, **kwargs)
+
+    @property
+    def use_hash(self):
+        return self._use_hash
+
+    @property
+    def hash_size(self):
+        return self._hash_size
+
+    @property
+    def emb_dim(self):
+        return self._emb_dim
